588f5139504ef84511d3ec668484675e766c4973
Md. Shamim Shahnewaz <mdshamimshahnewaz@gmail.com>
2017-10-16 02:13:58 +0600

    Testimonial added with functionality

--- functions.php
+++ functions.php
@@ -105,6 +105,13 @@ if ( ! function_exists( 'so_assets' ) ) {
 	
 	function so_assets() {
 		
+		$expertise = array();
+		if ( in_array( 'codestar-framework-1.0.1/cs-framework.php', get_option( 'active_plugins' ) ) ) {
+			foreach ( cs_get_option( 'so_section_home_expertise' ) as $key => $value ) {
+				$expertise[] = $value[ 'so_section_home_expertise_title' ];
+			}
+		}
+		
 		wp_enqueue_style( 'so-style', DIR_SHAMS_ONE_STYLE . '/style.css' );
 		wp_enqueue_script( 'jquery' );
 		wp_enqueue_script( 'so-bootstrap', DIR_SHAMS_ONE_JS . 'bootstrap.min.js', array( 'jquery' ), '4.8.2', TRUE );
@@ -116,6 +123,7 @@ if ( ! function_exists( 'so_assets' ) ) {
 		wp_enqueue_script( 'so-jquery-magnific-popup', DIR_SHAMS_ONE_JS . 'jquery.magnific-popup.min.js', array( 'jquery' ), '4.8.2', TRUE );
 		wp_enqueue_script( 'so-google-map', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAlXvhmyhieCZuRGoZMZPVefRFd4HpOxe0', array( 'jquery' ), '4.8.2', TRUE );
 		wp_enqueue_script( 'so-main', DIR_SHAMS_ONE_JS . 'main.js', array( 'jquery' ), '4.8.2', TRUE );
+		wp_localize_script( 'so-main', 'expertise', $expertise );
 	}
 	
 	add_action( 'wp_enqueue_scripts', 'so_assets' );
