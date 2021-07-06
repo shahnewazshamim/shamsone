6343c7a92793e55d97da59fa8d341e9fbf80ff36
Md. Shamim Shahnewaz <mdshamimshahnewaz@gmail.com>
2021-07-07 02:51:34 +0600

    main page template added

--- page-template/shamim-main.php
+++ page-template/shamim-main.php
@@ -1,1 +1,19 @@
 <?php
+/**
+ * Template Name: Main Page
+ *
+ * @package    WordPress
+ * @subpackage Shamim_Ninja
+ * @since      Shamim Ninja 1.0.0
+ */
+?>
+
+<?php get_header(); ?>
+
+<?php //get_template_part( 'template-parts/content', 'navbar' ); ?>
+
+
+
+<?php //get_template_part( 'template-parts/content', 'footer' ); ?>
+
+<?php get_footer(); ?>
